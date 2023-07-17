2351c0db0388d536b8c17088b8e2986ec5693c1d
Kirill <forworkskirill@gmail.com>
2023-07-17 17:09:35 +0500

    create protected route

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -7,6 +7,7 @@ import Login from "../Login/Login";
 import MyProfile from "../MyProfile/MyProfile";
 
 import PopupEditAccount from "../PopupEditAccount/PopupEditAccount";
+import ProtectedRoute from "../ProtectedRoute";
 function App() {
   const [isOpen, setIsOpen] = useState(false);
   function handleClosePopup() {
@@ -23,8 +24,16 @@ function App() {
         <Route path="/sign-in" element={<Login />} />
         <Route
           path="/my-profile"
-          element={<MyProfile onOpen={handleOpenPopup} />}
+          element={
+            <ProtectedRoute>
+              <MyProfile onOpen={handleOpenPopup} />
+            </ProtectedRoute>
+          }
         />
+        {/* <Route
+          path="/my-profile"
+          element={<MyProfile onOpen={handleOpenPopup} />}
+        /> */}
       </Routes>
       <PopupEditAccount isOpen={isOpen} onClose={handleClosePopup} />
     </div>
